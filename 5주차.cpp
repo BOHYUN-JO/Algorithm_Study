122257db578caa4254d24a04a016afbf46d6d555
BOHYUN-JO <whqhgus12@gmail.com>
2021-09-03 14:48:48 +0900

    2021-09-03 my birthdaygit add .

---
+++ 5주차.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+int solution(string word) {
+    int answer = 0;
+    vector<char> v;
+    vector<int> ind(25,0);
+    char arr[5] = {'A','E','I', 'O', 'U'};
+    set<string> dict;
+    
+    ind[24] = 1;
+    sort(ind.begin(), ind.end());
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            v.push_back(arr[i]);
+        }
+    }
+    
+    for(int i=0; i<5; i++){
+        do{
+           string temp = "";
+           for(int j=0; j<ind.size(); j++){
+               if(ind[j] == 1){
+                   temp += v[j];                
+               }
+           }
+           sort(temp.begin(), temp.end());
+           do{
+               dict.insert(temp);
+           }while(next_permutation(temp.begin(), temp.end()));
+        }while(next_permutation(ind.begin(), ind.end()));
+        ind[0] = 1;
+        sort(ind.begin(), ind.end());
+    }
+    
+    for(auto it = dict.begin(); it!=dict.end(); it++){
+        answer++;
+        if(*it == word){
+            break;
+        }
+    }
+    
+    return answer;
+}
