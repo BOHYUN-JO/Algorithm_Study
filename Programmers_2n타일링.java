123967a9f88032aa29748f45aa5e1dc197c18c4c
Bohyun-Jo <whqhgus12@gmail.com>
2022-07-21 21:10:26 +0900

    auto push

---
+++ Programmers_2n타일링.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Programmers_2n≈∏¿œ∏µ {
+    static final int MOD = 1000000007;
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) %MOD;
+        }
+        
+        return dp[n];
+    }
+}
