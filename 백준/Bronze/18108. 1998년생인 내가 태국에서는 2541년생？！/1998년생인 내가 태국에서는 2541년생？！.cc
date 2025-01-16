dd94a989f8c3f9e04dcd60f959d47ffb3f23b2b7
BOHYUN-JO <70252973+BOHYUN-JO@users.noreply.github.com>
2025-01-16 21:17:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 입출력 최적화
+void fast_io()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+}
+
+// 유틸리티 함수들
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+// 디버깅용 매크로 (필요에 따라 주석 처리 가능)
+#define debug(x) cerr << #x << " = " << (x) << endl
+
+// 상수들
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+// 문제 해결 함수
+void solve()
+{
+    int y;
+    cin >> y;
+    cout << y-543 << endl;
+}
+
+int main()
+{
+  fast_io();
+
+  int t = 1;
+
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
